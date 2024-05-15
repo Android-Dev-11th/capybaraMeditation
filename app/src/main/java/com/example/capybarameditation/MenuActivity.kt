1168a14d26e3d83ce2af87d5b5828b5429aeb29a
saltine1 <sophialiu.mail@gmail.com>
2024-05-15 10:36:34 -0700

    day 5: intents for menu button

--- app/src/main/java/com/example/capybarameditation/MenuActivity.kt
+++ app/src/main/java/com/example/capybarameditation/MenuActivity.kt
@@ -1,5 +1,6 @@
 package com.example.capybarameditation
 
+import android.content.Intent
 import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
@@ -14,7 +15,33 @@ class MenuActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMenuBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        binding.button2.setOnClickListener {
+            val meditationIntent = Intent(this, MeditationActivity::class.java)
+            meditationIntent.putExtra("meditationLength", 2)
+            startActivity(meditationIntent)
+        }
+
+        binding.button5.setOnClickListener {
+            val meditationIntent = Intent(this, MeditationActivity::class.java)
+            meditationIntent.putExtra("meditationLength", 5)
+            startActivity(meditationIntent)
+        }
+
+        binding.button10.setOnClickListener {
+            val meditationIntent = Intent(this, MeditationActivity::class.java)
+            meditationIntent.putExtra("meditationLength", 10)
+            startActivity(meditationIntent)
+        }
+
+        binding.accomplishments.setOnClickListener {
+            val accomplishmentIntent = Intent(this, AccomplishmentActivity::class.java)
+           startActivity(accomplishmentIntent)
+        }
     }
+
+
 
 
 
